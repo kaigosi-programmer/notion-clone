2e31402a3d62df3dc5964b8d64621f5323eeec10
HiroIrie <h0514c0325@gmail.com>
2023-09-22 00:26:23 +0900

    registerページ作成

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,21 @@
-import { Box, TextField } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import React from 'react';
+import{Link}from 'react-router-dom';
 
 function Register() {
   return (
-    <Box component="form">
+    <>
+     <Box component="form">
         <TextField fullWidth id="username" label="お名前" margin="normal" name="username" required/>
         <TextField fullWidth id="password" label="パスワード" margin="normal" name="password" required type="password"/>
         <TextField fullWidth id="confirmPassword" label="確認用パスワード" margin="normal" name="password" required type="password"/>
-        <LoadingButton></LoadingButton>
+        <LoadingButton sx={{mt:3,mb:2,}}fullWidth type="submit" loading={false} color="primary" variant="outlined">アカウント作成</LoadingButton>
+        
     </Box>
+    <Button component={Link} to="/login">すでにアカウントを持っていますか❔ログイン</Button>
+    </>
+   
   )
 }
 
